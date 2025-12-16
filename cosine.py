5447d22e778b98f3a123340d279fda46ebcd864e
akostin1 <akostin@haverford.edu>
2025-12-16 12:07:19 -0500

    Update cosine.py

--- cosine.py
+++ cosine.py
@@ -29,13 +29,12 @@ def load_songs_csv(file_path: str):
     return pd.read_csv(file_path)
 songs = load_songs_csv("songs.csv")
 
-#function to load cos_ranked_pairs data as a workable dataframe "ranked_pairs"
+#function to load cos_ranked_pairs CSV as a workable dataframe "ranked_pairs"
 @st.cache_data
-def load_cos_data():
-    url = "https://huggingface.co/datasets/akostin1/cos_ranked_pairs/resolve/main/cos_ranked_pairs.parquet"
-    return pd.read_parquet(url)
-
-ranked_pairs = load_cos_data()
+def load_cos_parquet(file_path: str):
+    """Load parquet file and cache the result."""
+    return pd.read_parquet(file_path)
+ranked_pairs = load_cos_parquet("cos_ranked_pairs.parquet")
 
 
 # ___________________________________________________________________________________________
